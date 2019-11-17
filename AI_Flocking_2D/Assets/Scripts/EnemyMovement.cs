c47f24e00bfe6b1e7b23c867abf794dfc3b72199
omg3011 <joeljoel3011@gmail.com>
2019-11-17 20:43:51 +0800

    Failed my AI Flocking

--- AI_Flocking_2D/Assets/Scripts/EnemyMovement.cs
+++ AI_Flocking_2D/Assets/Scripts/EnemyMovement.cs
@@ -4,83 +4,134 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    //-- Prefab
-    public Transform target;
+  [Header("Parenting")]
+  public List<Transform> ListOfAgent;
 
-    //-- Twerkable
-    public float speed = 5;
-    public float dodgeWeight = 3;
-    public float detection_range;
-    public float fov_angle = 90;
-    public Vector3 moveDir;
-    Vector3 originalDir;
+  [Header("Basic Settings")]
+  public float speed = 5.0f;
+  public float DETECt_RANGE = 5;
+
+  [Header("Weights")]
+  public float alignmentWeight = 1;
+  public float cohesionWeight = 1;
+  public float separationWeight = 1;
 
 
-    private void Start()
+  [Header("Move Dir")]
+  public Vector2 moveDir = Vector2.left;
+
+  [HideInInspector]
+  public Vector2 velocity;
+
+  // Alignment: Get the avereage velocity
+  Vector2 computeAlignment(Transform myAgent)
+  {
+    Vector2 result = Vector3.zero;
+    int neighbourCount = 0;
+
+    foreach (Transform otherT in ListOfAgent)
     {
-        originalDir = moveDir;
-    }
-
-    void Test1()
-    {
-        Vector3 headingVector = transform.position - target.position;
-
-        float scale = headingVector.magnitude / detection_range;
-
-        moveDir += headingVector.normalized / scale;
-        Debug.Log("Siam ah");
-    }
-
-    void Test2()
-    {
-        Vector3 headingVector = transform.position - target.position;
-        //Vector3 headingVector = target.position - transform.position;
-        float len = headingVector.magnitude;
-
-        if(len < detection_range)
+      if(otherT != myAgent)
+      {
+        if(Vector2.Distance(myAgent.position, otherT.position) < DETECt_RANGE)
         {
-            Vector3 adj = headingVector.normalized;
-            float diff = (detection_range - len) * dodgeWeight;
-            adj *= diff;
-            moveDir -= adj;
-            moveDir.Normalize();
+          EnemyMovement enemy = otherT.GetComponent<EnemyMovement>();
+          result += enemy.velocity;
+          neighbourCount++;
         }
+      }
     }
 
-    bool IsInCone()
+    if (neighbourCount == 0)
+      return result;
+
+    result /= neighbourCount;
+    result.Normalize();
+
+    return result;
+  }
+
+
+  // Cohesion: Get the direction towards center of mass
+  Vector2 computeCohesion(Transform myAgent)
+  {
+    Vector2 result = Vector3.zero;
+    int neighbourCount = 0;
+
+    foreach (Transform otherT in ListOfAgent)
     {
-        Vector3 dir = target.position - transform.position;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-
-
-        Debug.Log("Angle: " + angle + ", Left: " + (transform.localEulerAngles.x - fov_angle / 2));
-        if(angle > transform.rotation.x - fov_angle/2)
+      if (otherT != myAgent)
+      {
+        if (Vector2.Distance(myAgent.position, otherT.position) < DETECt_RANGE)
         {
-            if (angle < transform.localEulerAngles.x + fov_angle / 2)
-                return true;
+          result.x += otherT.position.x;
+          result.y += otherT.position.y;
+          neighbourCount++;
         }
-
-        return false;
+      }
     }
 
+    if (neighbourCount == 0)
+      return result;
 
-    private void Update()
+    result /= neighbourCount;
+    result = new Vector2(result.x - myAgent.position.x, result.y - myAgent.position.y);
+    result.Normalize();
+
+    return result;
+  }
+
+
+  // Separation: Steer away from neighbour
+  Vector2 computeSeparation(Transform myAgent)
+  {
+    Vector2 result = Vector3.zero;
+    int neighbourCount = 0;
+
+    foreach (Transform otherT in ListOfAgent)
     {
-        moveDir = originalDir;
-        if (target)
+      if (otherT != myAgent)
+      {
+        if (Vector2.Distance(myAgent.position, otherT.position) < DETECt_RANGE)
         {
-            if (Vector3.Distance(target.position, transform.position) <= detection_range)
-            {
-                if(IsInCone())
-                {
-                    Debug.Log("In view");
-                }
-                //Test1();
-                //Test2();
-            }
+          result.x += otherT.position.x - myAgent.position.x;
+          result.y += otherT.position.y - myAgent.position.y;
+          neighbourCount++;
         }
-        moveDir.z = 0;  
-        transform.rotation = Quaternion.LookRotation(moveDir);
-        transform.position += moveDir * speed * Time.deltaTime;
+      }
     }
+
+    if (neighbourCount == 0)
+      return result;
+
+    result /= neighbourCount;
+    result *= -1;         // Move reverse
+    result.Normalize();
+
+    return result;
+  }
+
+  private void Update()
+  {
+    velocity = Vector2.zero;
+    if(ListOfAgent.Count == 0)
+    {
+      velocity += speed * moveDir;
+    }
+    else
+    {
+      Vector2 alignment = computeAlignment(transform) * alignmentWeight;
+      Vector2 cohesion = computeCohesion(transform) * cohesionWeight;
+      Vector2 separation = computeSeparation(transform) * separationWeight;
+
+      velocity += Vector2.one * speed + alignment + cohesion + separation;
+      Debug.Log("Alignment: " + alignment + ", Cohesion: " + cohesion + ", sep: " + separation + ", Vel: " + velocity);
+    }
+
+    transform.position += new Vector3(
+      velocity.x * Time.deltaTime,
+      velocity.x * Time.deltaTime,
+      transform.position.z
+      );
+  }
 }
